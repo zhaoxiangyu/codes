8083e9cbf405ae3d8178b61aa3ae2948d8027a85
blueocci <blueocci@hotmail.com>
2013-06-10 22:50:24 +0800

    android.sh

--- codes/start/android.sh
+++ codes/start/android.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-#comments go here
+
+/home/he/work/sw/java/adt-bundle-linux-x86-20130522/eclipse/eclipse -data /home/he/work/ws/adt-bundles &
+#firefox http://docs.wxwidgets.org/2.8.11/ &
+nautilus /media/sf_vmshare/code-repo/github/codes/android &
+gnome-terminal --working-directory=/media/sf_vmshare/code-repo/github/codes/android &
