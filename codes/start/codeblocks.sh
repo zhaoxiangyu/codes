b983d60b9166d9bf76a3507b44140609316de4e2
blueocci <blueocci@hotmail.com>
2013-06-10 17:16:13 +0800

    .gitignore added
    codeblocks.sh added

---
+++ codes/start/codeblocks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+codeblocks /media/sf_vmshare/code-repo/github/codes/c/wxWi/wxWi.cbp &
+firefox http://docs.wxwidgets.org/2.8.11/ &
+nautilus /media/sf_vmshare/code-repo/github/codes/c/wxWi &
+gnome-termial --working-directory=/media/sf_vmshare/code-repo/github/codes/c/wxWi &
