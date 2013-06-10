e04b0b5d6195a927beeedaff2aeb8cb053acb2e1
blueocci <blueocci@hotmail.com>
2013-06-10 17:48:30 +0800

    swing.sh start script

---
+++ codes/start/swing.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/home/he/work/sw/java/eclipse/eclipse -data /home/he/work/ws/eclipse4.2 &
+#firefox http://docs.wxwidgets.org/2.8.11/ &
+nautilus /media/sf_vmshare/code-repo/github/codes/pc/swing &
+gnome-terminal --working-directory=/media/sf_vmshare/code-repo/github/codes/pc/swing &
