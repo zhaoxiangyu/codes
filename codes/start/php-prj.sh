8f99dfee2cbb7c23eb05ff0bcd398c7b5ebb452a
blueocci <blueocci@hotmail.com>
2013-06-11 01:36:24 +0800

    php project

---
+++ codes/start/php-prj.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/home/he/work/sw/php/eclipse-php-helios-linux-gtk/eclipse/eclipse -data /home/he/work/ws/php-helios &
+#firefox http://docs.wxwidgets.org/2.8.11/ &
+nautilus /home/he/work/code-repo/sinaapp/wd4web &
+gnome-terminal --working-directory=/home/he/work/code-repo/sinaapp/wd4web &
