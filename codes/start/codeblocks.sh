a9263d49ad1e927b37a5201f841910f8676690c7
blueocci <blueocci@hotmail.com>
2013-06-11 01:26:59 +0800

    fix

--- codes/start/codeblocks.sh
+++ codes/start/codeblocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-codeblocks /media/sf_vmshare/code-repo/github/codes/c/wxWi/wxWi.cbp &
+codeblocks /home/he/work/code-repo/github/codes/c/wxWi/wxWi.cbp &
 firefox http://docs.wxwidgets.org/2.8.11/ &
 nautilus /home/he/work/code-repo/github/codes/c/wxWi &
 gnome-termial --working-directory=/home/he/work/code-repo/github/codes/c/wxWi &
