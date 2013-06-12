cc868036803d6de6d93e7b45c5cbb5c578de52ca
blueocci <blueocci@hotmail.com>
2013-06-12 15:03:57 +0800

    move source files to src dir

---
+++ codes/c/wxMedia/src/ui/wxMediaApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      wxMediaApp.h
+ * Purpose:   Defines Application Class
+ * Author:    he (hejack0207@sohu.com)
+ * Created:   2013-06-12
+ * Copyright: he ()
+ * License:
+ **************************************************************/
+
+#ifndef WXMEDIAAPP_H
+#define WXMEDIAAPP_H
+
+#include <wx/app.h>
+
+class wxMediaApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // WXMEDIAAPP_H
