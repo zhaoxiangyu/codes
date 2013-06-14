f20432fc7d64355c4152a756413d790bdafe0a55
blueocci <blueocci@hotmail.com>
2013-06-14 19:50:04 +0800

    add codeblocks workspace file

--- codes/start/codeblocks.sh
+++ codes/start/codeblocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-codeblocks /home/he/work/code-repo/github/codes/c/wxWi/wxWi.cbp /home/he/work/code-repo/github/codes/c/wxMedia/wxMedia.cbp &
+codeblocks /home/he/work/code-repo/github/codes/ws/codeblocks10.5/he.workspace &
 firefox http://docs.wxwidgets.org/2.8.11/ http://www.cmake.org/cmake/help/v2.8.7/cmake.html http://dranger.com/ffmpeg/tutorial02.html &
 nautilus /home/he/work/code-repo/github/codes/c/wxWi &
 gnome-termial --working-directory=/home/he/work/code-repo/github/codes/c/wxWi &
