e50dee882d75d77fa9be536824cccaa2121ec571
blueocci <blueocci@hotmail.com>
2014-01-22 01:09:01 +0800

    publish on 2014-01-22 01:09:01+08:00

--- codes/code-man.sh
+++ codes/code-man.sh
@@ -8,10 +8,22 @@ help(){
 
 install(){
 	echo running install ...
+	git config --global user.name "blueocci_ubuntu"
+	git config --global user.email "blueocci@hotmail.com"
+    cd ../..
+	mkdir "github-`date --rfc-3339=date`"
+	cd "github-`date --rfc-3339=date`"
+    git clone https://github.com/blueocci/codes.git
+	cd codes
+	git remote add upstream https://github.com/blueocci/codes.git
+	git fetch upstream
 }
 
 case "$1" in
-help|status)
+help)
+	git config -l
+	;;
+status)
 	help
 	;;
 install)
@@ -27,7 +39,7 @@ pub)
 	git push
 	;;
 *)
-	echo "Usage: $0 {help|status|install|gui}"
+	echo "Usage: $0 {help|status|install|gui|pub}"
 	exit 1
 esac
 
