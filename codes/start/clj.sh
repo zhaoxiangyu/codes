2b51c06718b2c9013d8c4e1cca3828c1351a615b
blueocci_ubuntu <blueocci@hotmail.com>
2014-07-29 19:57:28 +0800

    add clojure project

---
+++ codes/start/clj.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/home/he/work/sw/java/eclipse/eclipse -data /home/he/work/code-repo/github/codes/ws/eclipse4.2-clj &
+#firefox http://docs.wxwidgets.org/2.8.11/ &
+nautilus /home/he/work/code-repo/github/codes/clojure/parser &
+gnome-terminal --working-directory=/home/he/work/code-repo/github/codes/clojure/parser &
