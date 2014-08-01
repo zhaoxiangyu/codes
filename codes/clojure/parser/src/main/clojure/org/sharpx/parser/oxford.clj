585346bcb0e16d6b62711fca67ab7f983fa8852f
blueocci_ubuntu <blueocci@hotmail.com>
2014-08-01 22:43:11 +0800

    'oxford.clj'

--- codes/clojure/parser/src/main/clojure/org/sharpx/parser/oxford.clj
+++ codes/clojure/parser/src/main/clojure/org/sharpx/parser/oxford.clj
@@ -3,22 +3,36 @@
   ;  (:use [monger.core :only [connect get-db disconnect]])
   (:require [monger.core :as mg]
             [monger.collection :as mc])
+  (:use [clj-xpath.core :only [$x $x:tag $x:text $x:attrs $x:attrs* $x:node xml->doc]])
   (:gen-class))
 
-(defn -htmls [host port dbname cname]
-
+(defn -htmls [host port dbname cname parser]
+  "processing url:html pairs in mongo database"
   (let [conn (mg/connect {:host host :port port})
-        db (mg/get-db conn dbname)]
+        db (mg/get-db conn dbname)
+        rs (map parser (mc/find-maps db cname))]
+        ;rs (map (fn [m] [(:url m) (count (:html m))])
+        ;     (mc/find-maps db cname))]
+    ;rs (map #(:url %) (mc/find-maps db cname))]
+    (last rs)
+    (mg/disconnect conn)
+    rs
+    ))
 
-    (let [rs (mc/find-maps db cname)]
-      (doall (map println rs))
-      (mg/disconnect conn)
-      )))
-
-(defn -parse []
-  (let [htmls (-htmls "169.254.10.41" 27017 "oxford" "htmls")]
-    (println htmls)))
+;/html/body//div[contains(@id,"entryContent")]
+(defn -parse [{:keys [type url html]}]   ;[{t :type url :url html :html}]
+  "parse url:html pair"
+  (println type url (count html))
+  (comment
+    (if (= type "e")
+      (do (prn html)
+        (let [doc (xml->doc html)]
+          (prn ($x "//div[contains(@id,\"entryContent\")]" doc))))
+      (prn "index")))
+  [type url (count html)]
+  )
 
 (defn -main [& argv]
   (println "oxford dict parser started!")
-  (-parse))
+  (let [htmls (-htmls "169.254.10.41" 27017 "oxford" "htmls" -parse)]
+    (dorun (map println htmls))))
