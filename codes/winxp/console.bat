4ef9386888a39d516fe5e8413e4fc0e6ee898795
helong <hejack0207@sina.com>
2014-08-05 20:25:59 +0800

    bat-wrapper

--- codes/winxp/console.bat
+++ codes/winxp/console.bat
@@ -2,24 +2,29 @@
 REM cd /d E:\he-xun\prj\naomi-lzhd-storm
 cd /d E:\code-repo2\github\codes\winxp
 
-set PATH=%PATH%;"C:\Program Files\PuTTY"
-set PATH=%PATH%;"C:\Program Files\MySQL\MySQL Server 5.5\bin"
-set PATH=%PATH%;"E:\java"
-set PATH=%PATH%;"E:\tools"
-set PATH=%PATH%;"E:\tools\clink_0.4.2"
-set PATH="E:\tools\shell.w32-ix86";%PATH%
-set PATH=%PATH%;"C:\Program Files\Java\jdk1.6.0_45\bin"
-set PATH=%PATH%;"C:\Program Files\Lynx - web browser"
-REM @set PATH=%PATH%;"E:\tools\ConEmuPack.140703"
-set PATH="C:\Program Files\7-Zip";%PATH%
-set PATH="C:\Program Files\Vim\vim74";%PATH%
-set PATH="C:\Program Files\PSPad editor";%PATH%
-set PATH="C:\Program Files\Sublime Text 2";%PATH%
-set PATH="C:\Program Files\MongoDB 2.6 Standard\bin";%PATH%
+set PATH=%PATH%;C:\Program Files\PuTTY
+set PATH=%PATH%;C:\Program Files\MySQL\MySQL Server 5.5\bin
+set PATH=%PATH%;E:\tools
+set PATH=%PATH%;E:\tools\clink_0.4.2
+set PATH=E:\tools\shell.w32-ix86;%PATH%
+set PATH=%PATH%;C:\Program Files\Lynx - web browser
+REM @set PATH=%PATH%;E:\tools\ConEmuPack.140703
+set PATH=C:\Program Files\7-Zip;%PATH%
+set PATH=C:\Program Files\Vim\vim74;%PATH%
+set PATH=C:\Program Files\PSPad editor;%PATH%
+set PATH=C:\Program Files\Sublime Text 2;%PATH%
+set PATH=C:\Program Files\Windows Resource Kits\Tools\;%PATH%
 
-call "e:\he\py\tools-py.bat" setpath
+set PATH=C:\Program Files\MongoDB 2.6 Standard\bin;%PATH%
+
+call e:\he\py\tools-py.bat setpath
+
+set PATH=%PATH%;C:\Program Files\Java\jdk1.6.0_45\bin
 
 set HOME=E:\code-repo2\github\codes\winxp\user-home
+set PATH=%HOME%\bat-wrapper;%PATH%
+REM set USERPROFILE=E:\code-repo2\github\codes\winxp\user-home
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_65
 
-cmd.exe /s /k "clink.bat inject"
+set PATH=%PATH:"=%
+cmd.exe /s /k clink.bat inject
---
+++ codes/winxp/user-home/bat-wrapper/mvn.bat
@@ -0,0 +1,1 @@
+call E:\he-xun\sw\apache-maven-3.2.1\bin\mvn.bat -s%HOME%\.m2\settings.xml %*
