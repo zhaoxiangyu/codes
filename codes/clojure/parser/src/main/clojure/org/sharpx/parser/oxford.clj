a5f2befc8d23b385d200adea7c3e2ee31922e121
helong <hejack0207@sina.com>
2014-08-06 22:04:05 +0800

    oxford.clj

--- codes/clojure/parser/src/main/clojure/org/sharpx/parser/oxford.clj
+++ codes/clojure/parser/src/main/clojure/org/sharpx/parser/oxford.clj
@@ -5,8 +5,11 @@
             [monger.collection :as mc])
   (:use [org.sharpx.parser.htmlclean])
   (:use [clj-xpath.core :only [$x $x:tag $x:text $x:attrs $x:attrs* $x:node xml->doc]])
+  (:use clojure.java.browse)
   (:require [clojure.java.io :as io])
   (:import org.sharpx.utils.FsUtils)
+  (:import java.net.URL)
+  (:import java.io.File)
   (:import java.util.HashMap)
   (:gen-class))
 
@@ -14,21 +17,28 @@
 (defn -parse
   "parse url:html pair"
   ([{:keys [type url html]}] ;[{t :type url :url html :html}]
-    (->>
-      (if (= type "e")
-        (let [entry (->> (html-clean html)
-                      xml->doc
-                      ($x "//div[contains(@id,\"entryContent\")]"))]
-          (println entry)) ;#((println %) %)))
-        (prn "index"))
-      ;println
-      ;(str "aa")
-      (conj [type url (count html)])))
+    (let [new-values
+          (if (= type "e")
+            (let [doc (->> (html-clean html)
+                        xml->doc)
+                  entry ($x:text "//div[contains(@class,\"webtop-g\")]/h2" doc)
+                  pos ($x:text "//div[contains(@class,\"webtop-g\")]/span[contains(@class,\"pos\")]" doc)]
+              {:entry entry :pos pos}) ;#((println %) %)))
+            (prn "index"))]
+      (println new-values)
+      (merge {:type type :url url :bc (count html)} new-values)))
+
   ([filepath,dest-dir]
-    (let [fc (FsUtils/loadJson filepath (.getClass (HashMap.)) nil)
+    (let [fc (FsUtils/loadJson filepath (.getClass (HashMap.)) nil) ;(FsUtils/loadJson filepath (class HashMap) nil)
+          inf (io/file filepath)
+          fbn (->> (.getName inf) (re-find #"^[^\.]+"))
+          outf (io/file dest-dir fbn (str fbn ".html"))
           type (.get fc "html") ;type (:type fc)
           url (.get fc "url") ;url (:url fc)
           html (.get fc "type")]
+      (when-not (.exists outf)
+        (io/make-parents outf) (spit outf html))
+      (browse-url (.toString (.toURL outf)))
       ;(println "url:" url "html:" html)
       (-parse {:type type :url url :html html}))))
 
@@ -55,4 +65,4 @@
   #_ (let [htmls (-htmls "169.254.244.218" 27017 "oxford" "htmls" -parse)]
        (dorun (map println htmls)))
   #_ (rand-parse "g:\\web-dict\\oxford\\htmls" "g:\\web-dict\\oxford\\analysis")
-  (-parse "g:\\web-dict\\oxford\\htmls\\20-1000.json" "g:\\web-dict\\oxford\\analysis"))
+  (-parse "g:\\web-dict\\oxford\\htmls\\20-500.json" "g:\\web-dict\\oxford\\analysis"))
