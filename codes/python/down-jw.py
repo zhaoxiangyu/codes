4ca50a952270c4307b98f97654c9911e907653a2
blueocci_osx10.8 <blueocci@hotmail.com>
2014-09-09 23:45:52 +0800

    down-jw.py

--- codes/python/down-jw.py
+++ codes/python/down-jw.py
@@ -1,4 +1,6 @@
-import os,fnmatch,zipfile
+#!/usr/bin/python
+
+import os,fnmatch,zipfile,shutil
 import os.path as path
 from urllib import urlretrieve
 
@@ -10,16 +12,20 @@ def download():
 	for i in range(48):
 		#url=base_url+"/"+str(i+1)+".zip"
 		url=base_url+"/course"+str(i+1)+".zip"
-		print "downloading file:",url
-		to_file=save_to+"/course"+str(i+1)+".zip"
+		to_file=path.join(save_to,"course"+str(i+1)+".zip")
 		if not path.isfile(to_file):
+			print "downloading file:",url
 			urlretrieve(url,to_file)
 
-	for fp in os.listdir(save_to):
-		if fnmatch.fnmatch(fp,"*.zip"):
+	for fn in os.listdir(save_to):
+		if fnmatch.fnmatch(fn,"*.zip"):
+			fp=path.join(save_to,fn)
 			with zipfile.ZipFile(fp) as zf:
-				dest_dir=path.join(save_to,
-				zf.extractall(save_to)
+				dir_name,_=path.splitext(fp)
+				dest_dir=path.join(save_to,dir_name)
+				if path.isdir(dest_dir):
+					shutil.rmtree(dest_dir)		
+				zf.extractall(dest_dir)
 
 if __name__ == "__main__":
 	download()
