5ead24acfb5bcef314e831c82ac7c8b473b506ea
blueocci_ubuntu <blueocci@hotmail.com>
2014-11-01 13:05:31 +0800

    publish on

---
+++ codes/c/crossword/cbp/CMakeFiles/crosswords.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/crosswords.dir/src/angry.c.o"
+  "crosswords.pdb"
+  "crosswords"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/crosswords.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
