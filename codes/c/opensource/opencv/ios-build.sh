c3e0a6cdbd43e076153f061ac1698078ee1b261b
blueocci_osx <blueocci@hotmail.com>
2014-11-06 01:25:46 +0800

    publish on

--- codes/c/opensource/opencv/ios-build.sh
+++ codes/c/opensource/opencv/ios-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 genprj(){
-
+        test -d /Developer || sudo ln -s /Applications/Xcode.app/Contents/Developer/ /Developer
+        python platforms/ios/build_framework.py ios
 }
 
 if test 0 -eq $1; then
