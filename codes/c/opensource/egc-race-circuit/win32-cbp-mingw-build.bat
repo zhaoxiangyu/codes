bbab05ac6a884f55d0198e03635071c703564c04
helong <hejack0207@sina.com>
2014-11-08 13:15:02 +0800

    publish

--- codes/c/opensource/egc-race-circuit/win32-cbp-mingw-build.bat
+++ codes/c/opensource/egc-race-circuit/win32-cbp-mingw-build.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set TRACE=--trace
 set MAKE=mingw32-make.exe
 if ""%1"" == """" (
 	echo command options:genprj build clean
@@ -10,9 +11,10 @@ exit /b 0
 
 :genprj
 	echo generate win32-cbp-mingw project
+	test -f glut-3.7.6-bin\glut.h || mkdir glut-3.7.6-bin\GL & mv glut-3.7.6-bin\glut.h glut-3.7.6-bin\GL\
 	test -d build\win32-cbp-mingw || mkdir build\win32-cbp-mingw
 	pushd build\win32-cbp-mingw
-	cmake -G"CodeBlocks - MinGW Makefiles" ..\..
+	cmake -G"CodeBlocks - MinGW Makefiles" %TRACE% ..\..
 	popd
 exit /b 0
 
