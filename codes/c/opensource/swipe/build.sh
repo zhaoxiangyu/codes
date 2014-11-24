3ac43a773d498463700a5905e5c86cae9dc68f7c
blueocci_mint <blueocci@hotmail.com>
2014-11-24 22:00:15 +0800

    publish on Linux

--- codes/c/opensource/swipe/build.sh
+++ codes/c/opensource/swipe/build.sh
@@ -7,11 +7,30 @@ install(){
 run(){
 	echo running
 	test -f king.wav || curl -O http://facstaff.bloomu.edu/jtomlins/Sounds/king.wav
+	play king.wav
 	./swipe -r 50:400 -n -i king.wav
 }
 
+testmn(){
+	echo testing music notes
+	play 1C.wav
+	./swipe -r 50:400 -n -i 1C.wav
+}
+
+prepare(){
+	echo preparing wave files
+	for aif in music-note-audios/*.aif; do
+		sox $aif $(basename $aif .aif).wav
+	done
+}
+
+clean(){
+	echo cleaning wave files
+	rm [0-9]*.wav
+}
+
 if test 0 -eq $#; then
-	echo "usage: install run"
+	echo "usage: install run prepare testmn clean"
 else
 	eval $1
 fi
