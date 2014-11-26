ee1349b607eaac3dfbe652fd55ce17d82cb963b2
blueocci_osx <blueocci@hotmail.com>
2014-11-26 23:51:42 +0800

    publish on Darwin

--- codes/c/opensource/bullet-physics/build.py
+++ codes/c/opensource/bullet-physics/build.py
@@ -3,94 +3,94 @@ import sys,os,string,shutil
 from getopt import getopt
 
 def genprj(target):
-	print "cmaking on system:",sys.platform
-	build_dir,generator,options=parse_target(target)
-	cmake(build_dir,generator,options)
+    print "cmaking on system:",sys.platform
+    build_dir,generator,options=parse_target(target)
+    cmake(build_dir,generator,options)
 
 
 def build(target):
-	print "building on system:",sys.platform
-	build_dir,generator,options=parse_target(target)
+    print "building on system:",sys.platform
+    build_dir,generator,options=parse_target(target)
 
-	currdir = os.getcwd()
-	os.chdir(build_dir)
+    currdir = os.getcwd()
+    os.chdir(build_dir)
 
-	if "darwin" == sys.platform :
-            os.system('make -j4')
-        else:
-            os.system('cmake -b .')
+    if "darwin" == sys.platform :
+        os.system('make -j4')
+    else:
+        os.system('cmake -b .')
 
-	os.chdir(currdir)
+    os.chdir(currdir)
 
 def install(target):
-	build_dir,generator,options=parse_target(target)
+    build_dir,generator,options=parse_target(target)
 
-	currdir = os.getcwd()
-	os.chdir(build_dir)
+    currdir = os.getcwd()
+    os.chdir(build_dir)
 
-	if "darwin" == sys.platform :
-            os.system('sudo make install')
-        else:
-            print "not implemenetd!"
+    if "darwin" == sys.platform :
+        os.system('sudo make install')
+    else:
+        print "not implemenetd!"
 
-	os.chdir(currdir)
+    os.chdir(currdir)
 
 def clean(target):
-	print "cleaning on system:",sys.platform
-	build_dir,generator,options=parse_target(target)
-	shutil.rmtree(build_dir)
+    print "cleaning on system:",sys.platform
+    build_dir,generator,options=parse_target(target)
+    shutil.rmtree(build_dir)
 
 def parse_target(target):
-        build_dir=""
-        generator=""
-        options=""
-	if "darwin" == sys.platform :
-		if "xcode" == target:
-			build_dir="build/darwin/xcode"
-			generator="Xcode"
-		if "makefile" == target:
-			build_dir="build/darwin/makefile"
-			generator="Unix Makefiles"
-                        options="-DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON -DFRAMEWORK=ON -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/Library/Frameworks -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks -DBUILD_DEMOS:BOOL=OFF"
-	elif "linux2" == sys.platform :
-		if "cbp" == target:
-			build_dir="build/linux/cbp"
-			generator="CodeBlocks - Unix Makefiles"
+    build_dir=""
+    generator=""
+    options=""
+    if "darwin" == sys.platform :
+        if "xcode" == target:
+            build_dir="build/darwin/xcode"
+            generator="Xcode"
+        elif "makefile" == target:
+            build_dir="build/darwin/makefile"
+            generator="Unix Makefiles"
+            options="-DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON -DFRAMEWORK=ON -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/Library/Frameworks -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks -DBUILD_DEMOS:BOOL=OFF"
+    elif "linux2" == sys.platform :
+        if "cbp" == target:
+            build_dir="build/linux/cbp"
+            generator="CodeBlocks - Unix Makefiles"
 
-	return (build_dir,generator,options)
+    return (build_dir,generator,options)
 
 def cmake(target_dir,generator,options):
-	if not os.path.isdir(target_dir):
-		os.makedirs(target_dir)
+    if not os.path.isdir(target_dir):
+        os.makedirs(target_dir)
 
-	currdir = os.getcwd()
-	os.chdir(target_dir)
-	os.system('cmake -G"'+generator+'" '+currdir+' '+options)
-	os.chdir(currdir)
+        currdir = os.getcwd()
+        os.chdir(target_dir)
+        os.system('cmake -G"'+generator+'" '+currdir+' '+options)
+        os.chdir(currdir)
 
 if __name__ == "__main__":
-        options,remainder = getopt(sys.argv[1:],'t:bcg',['target=','build','clean','generate','install'])
+    options,remainder = getopt(sys.argv[1:],'t:bcg',['target=','build','clean','generate','install'])
 
-	if len(sys.argv) == 1:
-		print """command options:
-	-t --target=target
-	-b --build
-	-g --generate
-	-c --clean
-	-i --install
-	target can be one of xcode,cbp or makefile """
-	        print "sys.platform:",sys.platform
+    if len(sys.argv) == 1:
+        print """command options:
+    -t --target=target
+    -b --build
+    -g --generate
+    -c --clean
+    -i --install
+    target can be one of xcode,cbp or makefile """
+        print "sys.platform:",sys.platform
 
-        target=""
-	for opt,arg in options:
-		if opt in ('-t','--target'):
-			target=arg
-		elif opt in ('-b','--build'):
-			build(target)
-		elif opt in ('-c','--clean'):
-			clean(target)
-		elif opt in ('-g','--generate'):
-			genprj(target)
-		elif opt in ('-i','--install'):
-			install(target)
+    target=""
+    for opt,arg in options:
+        if opt in ('-t','--target'):
+            target=arg
+        elif opt in ('-b','--build'):
+            build(target)
+        elif opt in ('-c','--clean'):
+            clean(target)
+        elif opt in ('-g','--generate'):
+            genprj(target)
+        elif opt in ('-i','--install'):
+            install(target)
 
