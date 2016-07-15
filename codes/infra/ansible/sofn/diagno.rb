bc70fdd766012371c93a9316c8fb28b6d6fb1a92
hejack0207 <hejack0207@sina.com>
2016-07-15 19:35:42 +0800

    change diagno.rb check_inet

--- codes/infra/ansible/sofn/diagno.rb
+++ codes/infra/ansible/sofn/diagno.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby -w
 
 def setup
-	`ansible -u root -k -i hosts all -m setup`
+	`ansible -u root -k -i hosts all -m setup >setup.txt`
 end
 
 def ping
 	puts `ansible -u root -k -i hosts all -m ping`
+end
+
+def check_inet
+	puts `ansible -u root -k -i hosts all -m command -a 'ping -c 5 www.baidu.com'`
 end
