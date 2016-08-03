5030dccaae66bf4d38ba988834747b8705e1bb44
hejack0207 <hejack0207@sina.com>
2016-08-03 09:20:36 +0800

    test oracle installation

--- codes/infra/ansible/sofn/install.rb
+++ codes/infra/ansible/sofn/install.rb
@@ -17,12 +17,6 @@ class Install
 		puts `#{cmd}`
 	end
 
-	def setup
-		cmd = %Q/ansible-playbook -i #{@inventory} -t setup site.yml/
-		puts cmd
-		puts `#{cmd}`
-	end
-
 	def install_java8
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_java8 site.yml/
 		puts cmd
@@ -35,43 +29,22 @@ class Install
 		puts `#{cmd}`
 	end
 
-	def t_install_oracle11g
-		cmd = %Q/ansible-playbook -i #{@inventory} -t install_oracledb -l 192.168.122.109 site.yml/
+	def setup(limit)
+		cmd = %Q/ansible-playbook -i #{@inventory} --tags="setup" -l #{limit}" site.yml/
 		puts cmd
 		puts `#{cmd}`
 	end
 
-	def t_upload_files
-		cmd = %Q/ansible-playbook -i #{@inventory} -t copy_files -l 192.168.122.109 site.yml/
+	def upload_files(limit)
+		cmd = %Q/ansible-playbook -i #{@inventory} -t copy_files -l #{limit} site.yml/
 		puts cmd
 		puts `#{cmd}`
 	end
 
-	def t_autostart_sshd
-		cmd = %Q/ansible-playbook -i #{@inventory} -t setup_autostart_sshd -l 192.168.122.109 site.yml/
+	def install_oracle11g(limit)
+		cmd = %Q/ansible-playbook -i #{@inventory} -t install_oracledb -l #{limit} site.yml/
 		puts cmd
 		puts `#{cmd}`
 	end
 
-=begin
-	def t_setup
-		puts `ansible-playbook -i #{@inventory} -t setup --extra-vars "target=test" site.yml`
-	end
-
-	def setup
-		puts `ansible-playbook -i #{@inventory} -t setup --extra-vars "target=all" site.yml`
-	end
-
-	def t_install_java8
-		cmd = %Q/ansible-playbook -i #{@inventory} -t install_java8 --extra-vars "target=test" site.yml/
-		puts cmd
-		puts `#{cmd}`
-	end
-
-	def install_java8
-		cmd = %Q/ansible-playbook -i #{@inventory} -t install_java8 --extra-vars "target=java8" site.yml/
-		puts cmd
-		puts `#{cmd}`
-	end
-=end
 end
