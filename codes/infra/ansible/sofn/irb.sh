374551a6bd5d509703f4f57cbda1affd7a6eb963
hejack0207 <hejack@sina.com>
2016-08-16 21:03:18 +0800

    publish on Linux

--- codes/infra/ansible/sofn/irb.sh
+++ codes/infra/ansible/sofn/irb.sh
@@ -2,4 +2,5 @@
 
 #ruby -r ./vgrnt.rb -e 'up'
 #irb -r ./diagno.rb -r ./install.rb -r ./roles.rb -r ./vgrnt.rb
-irb -r ./diagno.rb -r ./install.rb -r ./roles.rb -r ./vgrnt.rb -r ./main.rb
+#irb -r ./diagno.rb -r ./install.rb -r ./roles.rb -r ./vgrnt.rb -r ./main.rb
+pry -r ./diagno.rb -r ./install.rb -r ./roles.rb -r ./vgrnt.rb -r ./main.rb
