b7b2e0a9bc7081b55533f42dfeba912e144ec590
hejack0207 <hejack0207@sina.com>
2016-08-16 17:21:11 +0800

    publish on Linux

--- codes/infra/ansible/sofn/diagno.rb
+++ codes/infra/ansible/sofn/diagno.rb
@@ -3,27 +3,32 @@
 class Diagno
 
 	def setup
-		`ansible -i #{@inventory} all -m setup >setup.txt`
+		run "ansible -i #{@inventory} all -m setup >setup.txt"
 	end
 
 	def ping
-		puts `ansible -i #{@inventory} all -m ping` # -vvvv
+		run "ansible -i #{@inventory} all -m ping"
 	end
 
 	def check_inet
-		puts `ansible -i #{@inventory} all -m command -a 'ping -c 5 www.baidu.com'`
+		run "ansible -i #{@inventory} all -m command -a 'ping -c 5 www.baidu.com'"
 	end
 
 	def check_hw
-		puts `ansible-playbook -i #{@inventory} -t diagnose site.yml`
+		run "ansible-playbook -i #{@inventory} -t diagnose site.yml"
 	end
 
 	def t_check_hw
-		puts `ansible-playbook -i #{@inventory} -t diagnose site.yml`
+		run "ansible-playbook -i #{@inventory} -t diagnose site.yml"
 	end
 
 	def initialize(env)
 		@inventory = env
+	end
+
+	def run(command)
+		puts command
+		system command
 	end
 
 	def interactive
--- codes/infra/ansible/sofn/install.rb
+++ codes/infra/ansible/sofn/install.rb
@@ -7,80 +7,72 @@ class Install
 
 	def install
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def upload_files
 		cmd = %Q/ansible-playbook -i #{@inventory} -t copy_files site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def install_java8
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_java8 site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 	
 	def install_oracle11g
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_oracledb site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def install_nexus
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_nexus -l code site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def install_nexus_service
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_nexus_service -l code site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def setup(limit)
 		cmd = %Q/ansible-playbook -i #{@inventory} --tags="setup" -l #{limit} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def upload_files(limit)
 		cmd = %Q/ansible-playbook -i #{@inventory} -t copy_files -l #{limit} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def install_oracle11g(limit)
 		cmd = %Q/ansible-playbook -i #{@inventory} -t install_oracledb -l #{limit} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def post_install_oracle11g(limit)
 		cmd = %Q/ansible-playbook -i #{@inventory} -t post_oracle_install -l #{limit} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def preview(tags,limit)
 		cmd = %Q/ansible-playbook --list-hosts --list-tags --list-tasks -i #{@inventory} -l #{limit} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def syntax(limit)
 		cmd = %Q/ansible-playbook --syntax-check -i #{@inventory} site.yml/
-		puts cmd
-		puts `#{cmd}`
+		run cmd
 	end
 
 	def dryrun(tags,limit)
 		cmd = %Q/ansible-playbook -C -i #{@inventory} -l #{limit} site.yml/
+		run cmd
+	end
+
+	def run(command)
 		puts cmd
-		puts `#{cmd}`
+		system command
 	end
 
 end
