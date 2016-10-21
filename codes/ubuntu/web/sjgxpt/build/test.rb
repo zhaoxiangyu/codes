a398bf3c44374de85bcdb065acfb7612dd7e3598
hejack0207 <hejack0207@sina.com>
2016-10-21 18:16:06 +0800

    publish on Linux

--- codes/ubuntu/web/sjgxpt/build/test.rb
+++ codes/ubuntu/web/sjgxpt/build/test.rb
@@ -6,6 +6,14 @@ class WebTest
     run("curl -v -X POST '#{@baseurl}/sofn-dgap-web/resource/list?start=1&length=10'")
   end
 
+  def ws_error_list
+    run("curl -v -X POST '#{@baseurl}/sofn-dgap-web/wsErrorStat/list?start=1&length=10'")
+  end
+
+  def res_dirs
+    run("curl -v -X POST '#{@baseurl}/sofn-dgap-web/resourceDirectory/all'")
+  end
+
   def open_front
     run("firefox 'http://localhost:8001/'")
   end
