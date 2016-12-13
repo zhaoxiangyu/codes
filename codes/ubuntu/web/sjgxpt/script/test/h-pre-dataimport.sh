0f9017146e5dad5672fedf8b9354f1125447eb5a
hejack0207 <hejack0207@sina.com>
2016-12-13 18:27:50 +0800

    publish on Linux

--- codes/ubuntu/web/sjgxpt/script/test/h-pre-dataimport.sh
+++
@@ -1,1 +0,0 @@
-http --verbose post http://localhost:10080/sofn-dgap-web/swagger-ui.html @data-import.xml
--- codes/ubuntu/web/sjgxpt/script/test/pre-dataimport.sh
+++ codes/ubuntu/web/sjgxpt/script/test/pre-dataimport.sh
@@ -3,3 +3,5 @@
 service_url=http://192.168.21.246:8062/sofn-dgap-pre/ws/dataImport
 
 curl -X POST -H "Content-Type: text/xml" -H "SOAPAction: \"\"" --data-binary @data-import.xml $service_url
+
+#http --verbose post $service_url @data-import.xml
