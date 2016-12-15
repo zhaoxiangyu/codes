59e4be033bf304bd1745e5d2e37cb4412101efdb
hejack0207 <hejack0207@sina.com>
2016-12-15 20:54:04 +0800

    publish on Linux

--- codes/ubuntu/web/sjgxpt/tm.sh
+++ codes/ubuntu/web/sjgxpt/tm.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
+tmux new-session -s sjgxpt -d
 #tmux set-window-option -g automatic-rename off
-tmux new-session -s sjgxpt -n deploy -d
 #tmux split-window -v 'ipython'
 #tmux split-window -h
-tmux new-window -n test
-#tmux attach-session -t sjgxpt
+tmux new-window -t sjgxpt -c /home/helong/he/lky/share/sjgxpt/script/dp
+tmux new-window -t sjgxpt -c /home/helong/he/lky/share/sjgxpt/script/test
+tmux attach-session -t sjgxpt
 #tmux -2 attach-session -d
