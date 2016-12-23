94a05c84e9dc2e28f83c15e629406591d5d57d5a
hejack0207 <hejack0207@sina.com>
2016-12-23 19:15:19 +0800

    publish on Linux

--- codes/ubuntu/web/sjgxpt/script/dp/rb/lsvr.rb
+++ codes/ubuntu/web/sjgxpt/script/dp/rb/lsvr.rb
@@ -12,6 +12,8 @@ class Lsvr
 		@sso_service={tomcat_home:@tomcat_homes+"/sso-21080/"+@tomcat_dirname,app_dir:@app_dirs+"/sofn-sso-service/target/sofn-sso-service"}
 		@dgap_web={tomcat_home:@tomcat_homes+"/web-10080/"+@tomcat_dirname,app_dir:@app_dirs+"/sofn-dgap-web/target/sofn-dgap-web"}
 		@dgap_pre={tomcat_home:@tomcat_homes+"/pre-11080/"+@tomcat_dirname,app_dir:@app_dirs+"/sofn-dgap-pre/target/sofn-dgap-pre"}
+		#@servers = [@dgap_service,@sso_service,@dgap_web,@dgap_pre]
+		@servers = [@dgap_service,@dgap_pre]
 	end
 
 	def start_tomcat(tomcat_home)
@@ -24,27 +26,36 @@ class Lsvr
 
 	def start()
 		run "/home/helong/he/lky/share/sjgxpt/udev/sw/zookeeper-3.4.6/bin/zkServer.sh start"
-		start_tomcat @sso_service[:tomcat_home]
-		start_tomcat @dgap_service[:tomcat_home]
-		start_tomcat @dgap_web[:tomcat_home]
-		start_tomcat @dgap_pre[:tomcat_home]
+		#start_tomcat @sso_service[:tomcat_home]
+		#start_tomcat @dgap_service[:tomcat_home]
+		#start_tomcat @dgap_web[:tomcat_home]
+		#start_tomcat @dgap_pre[:tomcat_home]
+		@servers.each do |server|
+			start_tomcat server[:tomcat_home]
+		end
 	end
 
 	def kill()
 		run "pkill -f zookeeper-3.4.6"
-		kill_tomcat @sso_service[:tomcat_home]
-		kill_tomcat @dgap_service[:tomcat_home]
-		kill_tomcat @dgap_web[:tomcat_home]
-		kill_tomcat @dgap_pre[:tomcat_home]
+		#kill_tomcat @sso_service[:tomcat_home]
+		#kill_tomcat @dgap_service[:tomcat_home]
+		#kill_tomcat @dgap_web[:tomcat_home]
+		#kill_tomcat @dgap_pre[:tomcat_home]
+		@servers.each do |server|
+			kill_tomcat server[:tomcat_home]
+		end
 	end
 
 	def view_logs()
 		if not run "tmux has-session -t server_logs"
 			run "tmux new-session -s server_logs -d"
-			run "tmux new-window -t server_logs -n sso tail -f #{@sso_service[:tomcat_home]}/logs/catalina.out"
-			run "tmux new-window -t server_logs -n service tail -f #{@dgap_service[:tomcat_home]}/logs/catalina.out"
-			run "tmux new-window -t server_logs -n web tail -f #{@dgap_web[:tomcat_home]}/logs/catalina.out"
-			run "tmux new-window -t server_logs -n pre tail -f #{@dgap_pre[:tomcat_home]}/logs/catalina.out"
+			#run "tmux new-window -t server_logs -n sso tail -f #{@sso_service[:tomcat_home]}/logs/catalina.out"
+			#run "tmux new-window -t server_logs -n service tail -f #{@dgap_service[:tomcat_home]}/logs/catalina.out"
+			#run "tmux new-window -t server_logs -n web tail -f #{@dgap_web[:tomcat_home]}/logs/catalina.out"
+			#run "tmux new-window -t server_logs -n pre tail -f #{@dgap_pre[:tomcat_home]}/logs/catalina.out"
+			@servers.each do |server|
+				run "tmux new-window -t server_logs tail -f #{server[:tomcat_home]}/[l]ogs/catalina.out"
+			end
 		end
 		run "tmux attach-session -t server_logs"
 	end
