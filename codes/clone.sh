c362f6f1fd354540d387f66c0074a22cad33976b
he_fedora <blueocci@hotmail.com>
2016-12-29 07:37:18 +0800

    publish on Linux

---
+++ codes/clone.sh
@@ -0,0 +1,6 @@
+echo running install ...
+git config --global user.name "$USERNAME_$(lsb_release -si|tr A-Z a-z)"
+git config --global user.email "blueocci@hotmail.com"
+dirname="github-`date --rfc-3339=date`"
+mkdir $dirname
+git clone https://github.com/blueocci/codes.git $dirname
