ce188e4b23d3f092c8ebcf297485b57fe0a9cd0e
he_fedora <blueocci@hotmail.com>
2016-12-29 07:39:06 +0800

    publish on Linux

--- codes/clone.sh
+++ codes/clone.sh
@@ -1,4 +1,5 @@
 echo running install ...
+git config --global push.default simple
 git config --global user.name "$USERNAME_$(lsb_release -si|tr A-Z a-z)"
 git config --global user.email "blueocci@hotmail.com"
 dirname="github-`date --rfc-3339=date`"
