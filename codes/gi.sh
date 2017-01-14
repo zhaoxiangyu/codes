b9e651c2b8025391e9c1393a6b859108d727374a
blueocci_ubuntu <blueocci@hotmail.com>
2017-01-15 05:17:52 +0800

    publish on Linux

--- codes/gi.sh
+++ codes/gi.sh
@@ -1,12 +1,31 @@
 #/usr/bin/env bash
 
-set -ex
+#set -ex
+set -e
 PREFIX="https://raw.githubusercontent.com/blueocci/codes/master/codes"
 
 ginstall(){
 	local file=${1:?"you must set file path."}
+	local install_dir=${2:-"/usr/local/bin"}
 	local url="$PREFIX"/"$file"
-	curl -o $(basename $url) $url
+	local filename=$(basename $url)
+	local local_file=/tmp/$(basename $url)
+	local target_filename=${filename%%.*}
+
+	curl -o $local_file $url
+	sudo install $local_file $install_dir/$target_filename
 }
 
-ginstall 'ruby/rlwr.rb'
+confirm(){
+	local cmd="$*"
+	echo "command:$cmd"
+	read -p "Are you sure to run above command?" ans
+	case "$ans" in
+		y|Y)
+			$cmd ;;
+		n|N)
+			echo "quitting..." ;;
+	esac
+}
+
+ginstall 'ruby/yd.rb'
