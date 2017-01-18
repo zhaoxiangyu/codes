95de87af947bcd5699edf23a87421e87e744e271
hejack0207 <hejack0207@sina.com>
2017-01-18 11:28:01 +0800

    publish on Linux

--- codes/install.sh
+++ codes/install.sh
@@ -3,3 +3,5 @@ ln -fs $(readlink -f code-man.sh) ~/
 ln -fs $(readlink -f ubuntu/ci.sh) ~/
 ln -fs $(readlink -f ubuntu/co.sh) ~/
 ln -fs $(readlink -f gi.sh) ~/
+
+sudo curl -o /usr/local/bin/desk https://raw.githubusercontent.com/jamesob/desk/master/desk; sudo chmod +x /usr/local/bin/desk
