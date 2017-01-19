707cc642222c10c543723c8666ec257cd4ae9e70
he manjarolinux <blueocci@hotmail.com>
2017-01-19 22:09:38 +0800

    publish on Linux

--- codes/console/java-tools/build.sbt
+++ codes/console/java-tools/build.sbt
@@ -1,6 +1,6 @@
 organization := "org.sharpx"
 name := "java-tools"
 version := "0.1-SNAPSHOT"
-scalaVersion := "2.10.5"
+scalaVersion := "2.11.4"
 
 // codequality.CodeQualityPlugin.Settings
--- codes/ubuntu/ci.sh
+++ codes/ubuntu/ci.sh
@@ -10,15 +10,32 @@ target_dir="$basedir/user-home"
 sync="rsync -av --delete"
 #sync="install -D"
 
-$sync ~/.zshrc $target_dir/oh-my-zsh.zshrc
-$sync ~/.vimrc $target_dir/vundle.vimrc
-$sync ~/.emacs.d/ $target_dir/emacs.d
+zsh(){
+	$sync ~/.zshrc $target_dir/oh-my-zsh.zshrc
+}
 
-mkdir -p $target_dir/sbt/0.13/plugins
-$sync ~/.sbt/0.13/plugins/npt.sbt $target_dir/sbt/0.13/plugins/npt.sbt
-$sync ~/.sbt/0.13/plugins/plugins.sbt $target_dir/sbt/0.13/plugins/plugins.sbt
+vim(){
+	$sync ~/.vimrc $target_dir/vundle.vimrc
+}
 
-$sync ~/.coloritrc $target_dir/coloritrc
+emacs(){
+	$sync ~/.emacs.d/ $target_dir/emacs.d
+}
 
-mkdir -p $target_dir/desk
-rsync -av --delete --exclude=".vim/plugged" --exclude=".vim/dirs" --delete-excluded ~/.desk/ $target_dir/desk
+sbt(){
+	mkdir -p $target_dir/sbt/0.13/plugins
+	$sync ~/.sbt/0.13/plugins/npt.sbt $target_dir/sbt/0.13/plugins/npt.sbt
+	$sync ~/.sbt/0.13/plugins/plugins.sbt $target_dir/sbt/0.13/plugins/plugins.sbt
+	$sync ~/.sbt/repositories $target_dir/sbt/
+}
+
+colorit(){
+	$sync ~/.coloritrc $target_dir/coloritrc
+}
+
+desk(){
+	mkdir -p $target_dir/desk
+	rsync -av --delete --exclude=".vim/plugged" --exclude=".vim/dirs" --delete-excluded ~/.desk/ $target_dir/desk
+}
+
+$*
